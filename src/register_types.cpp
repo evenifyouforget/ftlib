4ee11c5f6b08b3b88e8fb7858bbfd7d9293c2d44
evenifyouforget <idiotvtuber@gmail.com>
2025-06-30 13:56:09 -0400

    bring empika's progress

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -2,12 +2,14 @@
 
 #include "core/object/class_db.h"
 #include "summator.h"
+#include "render.h"
 
 void initialize_ftlib_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
 	ClassDB::register_class<Summator>();
+    ClassDB::register_class<FTRender>();
 }
 
 void uninitialize_ftlib_module(ModuleInitializationLevel p_level) {
