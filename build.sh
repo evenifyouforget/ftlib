425ae09d7286d17d4ed8904f622f59636ce8b387
evenifyouforget <idiotvtuber@gmail.com>
2025-06-30 17:04:29 -0400

    remove build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-rm -rf build/
-mkdir -p build/modules/ftlib/
-cp -r src/. build/modules/ftlib/
-mv build/modules/ftlib/godot_adapter/config.py build/modules/ftlib/
-mv build/modules/ftlib/godot_adapter/register_types.h build/modules/ftlib/
-mv build/modules/ftlib/godot_adapter/SCsub build/modules/ftlib/
-cd godot
-scons custom_modules=../build/modules
