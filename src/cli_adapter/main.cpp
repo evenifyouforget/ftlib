77eee7616f5e2d45b1fd0ac4a659ba1b728bc264
evenifyouforget <idiotvtuber@gmail.com>
2025-07-02 16:26:55 -0400

    fix piece type in run_design_single

--- src/cli_adapter/main.cpp
+++ src/cli_adapter/main.cpp
@@ -2,7 +2,10 @@
 #include "fcsim.h"
 
 std::istream& operator>>(std::istream& is, fcsim_block_def& block) {
-    return is >> block.type >> block.id >> block.x >> block.y >> block.w >> block.h >> block.angle >> block.joints[0] >> block.joints[1];
+    int64_t block_type;
+    is >> block_type >> block.id >> block.x >> block.y >> block.w >> block.h >> block.angle >> block.joints[0] >> block.joints[1];
+    block.type = static_cast<fcsim_piece_type::type>(block_type);
+    return is;
 }
 
 std::istream& operator>>(std::istream& is, fcsim_rect& rect) {
