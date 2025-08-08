a31ecc7dc87b2e36aeec3d88e2453b9af8a1b14d
evenifyouforget <idiotvtuber@gmail.com>
2025-08-08 12:24:10 -0400

    use fixed paths

--- test/test_self_dirs.py
+++ test/test_self_dirs.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 # are we in the right directory?
 def test_run_from_right_dir():
-    test_dir = Path() / 'test'
+    test_dir = Path() / 'test' # intentionally not relative to the file
     assert test_dir.is_dir()
     conftest_path = test_dir / 'conftest.py'
     assert conftest_path.is_file()
