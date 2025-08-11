1582d941c62c31eb6969c22e6a6990635fcc9992
evenifyouforget <idiotvtuber@gmail.com>
2025-08-11 18:40:27 -0400

    basic setup

---
+++ test/save_design.py
@@ -0,0 +1,9 @@
+from typing import Optional
+from get_design import FCDesignStruct
+
+def save_design(design_struct: FCDesignStruct) -> Optional[int]:
+    """
+    Upload a design to the FC servers.
+    If it is successful, it will return the new design ID.
+    """
+    pass
